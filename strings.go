f3bdf1d890c4f42164eab82a0fa4d94ce545d4ca
themester <dgrripoll@gmail.com>
2018-09-11 12:14:16 +0200

    Added extensions string

--- strings.go
+++ strings.go
@@ -1,17 +1,18 @@
 package fastws
 
 var (
-	originString      = []byte("Origin")
-	connectionString  = []byte("Connection")
-	upgradeString     = []byte("Upgrade")
-	wsString          = []byte("websocket")
-	commaString       = []byte(",")
-	wsHeaderVersion   = []byte("Sec-WebSocket-Version")
-	wsHeaderKey       = []byte("Sec-WebSocket-Key")
-	wsHeaderProtocol  = []byte("Sec-Websocket-Protocol")
-	wsHeaderAccept    = []byte("Sec-Websocket-Accept")
-	uidKey            = []byte("258EAFA5-E914-47DA-95CA-C5AB0DC85B11")
-	supportedVersions = [][]byte{ // must be slice for future implementations
+	originString       = []byte("Origin")
+	connectionString   = []byte("Connection")
+	upgradeString      = []byte("Upgrade")
+	wsString           = []byte("websocket")
+	commaString        = []byte(",")
+	wsHeaderVersion    = []byte("Sec-WebSocket-Version")
+	wsHeaderKey        = []byte("Sec-WebSocket-Key")
+	wsHeaderProtocol   = []byte("Sec-Websocket-Protocol")
+	wsHeaderAccept     = []byte("Sec-Websocket-Accept")
+	wsHeaderExtensions = []byte("Sec-WebSocket-Extensions")
+	uidKey             = []byte("258EAFA5-E914-47DA-95CA-C5AB0DC85B11")
+	supportedVersions  = [][]byte{ // must be slice for future implementations
 		[]byte("13"),
 	}
 )
