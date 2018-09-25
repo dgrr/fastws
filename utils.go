f2932a43923c60d3d1713d4e84dbec35c1d8017b
themester <dgrripoll@gmail.com>
2018-09-25 12:40:22 +0200

    Added utils function to compare bytes equality

--- utils.go
+++ utils.go
@@ -29,3 +29,19 @@ func s2b(s string) []byte {
 	}
 	return *(*[]byte)(unsafe.Pointer(&bh))
 }
+
+func equalFold(b, s []byte) (equals bool) { // TODO: To asm
+	n := len(b)
+	if n != len(s) {
+		equals = false
+	} else {
+		equals = true
+		for i := 0; i < n; i++ {
+			if b[i]|0x20 != s[i]|0x20 {
+				equals = false
+				break
+			}
+		}
+	}
+	return
+}
